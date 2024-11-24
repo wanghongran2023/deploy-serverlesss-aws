f45d3d5ba2980fe1eb0e545d9453145df164562b
Ubuntu <ubuntu@ip-192-168-55-188.ap-northeast-1.compute.internal>
2024-11-24 12:11:07 +0000

    add delete functiomn

--- starter/backend/src/lambda/http/deleteTodo.js
+++ starter/backend/src/lambda/http/deleteTodo.js
@@ -1,8 +1,35 @@
+import { DynamoDB } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
 
-export function handler(event) {
-  const todoId = event.pathParameters.todoId
+const dynamoDbDocument = DynamoDBDocument.from(new DynamoDB());
+const todoTable = process.env.TODO_TABLE;
 
-  // TODO: Remove a TODO item by id
-  return undefined
+export async function handler(event) {
+	const todoId = event.pathParameters.todoId
+
+	try {
+		const result = await dynamoDbDocument.delete({
+			TableName: todoTable,
+            		Key: {
+				userId: "test",
+				todoId: todoId
+			}
+		});
+
+		return {
+			statusCode: 200,
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Credentials': true,
+			},
+			body: JSON.stringify({ message: 'Item deleted successfully', result })
+		};
+	} catch (error) {
+		console.error('Error deleting item from DynamoDB:', error);
+		return {
+			statusCode: 500,
+			body: JSON.stringify({ message: 'Internal Server Error' })
+		};
+	}
 }
 
