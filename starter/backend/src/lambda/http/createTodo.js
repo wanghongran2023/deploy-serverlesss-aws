32546b935b95a3ce656122c027461fe834651354
Ubuntu <ubuntu@ip-192-168-55-188.ap-northeast-1.compute.internal>
2024-11-24 16:48:23 +0000

    fix create ToDo

--- starter/backend/src/lambda/http/createTodo.js
+++ starter/backend/src/lambda/http/createTodo.js
@@ -19,7 +19,7 @@ export async function handler(event) {
   	const newTodo ={
 		userId: userId,
 		todoId: todoId,
-		done: false
+		done: false,
 		createdAt: new Date().toISOString(),
 		attachmentUrl: 'https://'+bucketName+'.s3.amazonaws.com/'+todoId+'.png',
 		...newTodoContent
